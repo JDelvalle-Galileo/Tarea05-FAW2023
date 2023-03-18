899652555fe4307bf361acb52c25b29d4b57189a
JDelvalle-Galileo <julio.delvalle@galileo.edu>
2023-03-17 19:52:26 -0600

    finish personajes and add style to all

--- src/app/component/personajes-display/personajes-display.component.ts
+++ src/app/component/personajes-display/personajes-display.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PersonajeSelectorService } from 'src/app/services/personaje-selector.service';
 
 @Component({
   selector: 'app-personajes-display',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./personajes-display.component.scss']
 })
 export class PersonajesDisplayComponent {
+  personaje:string = "";
+  imgSrc:string = ""
+
+  constructor(private selector:PersonajeSelectorService){
+    
+  }
+  
+  ngOnInit(){
+    this.selector.currentPersonaje.subscribe(personaje => this.personaje = personaje);
+    this.selector.currentImagen.subscribe(imagen => this.imgSrc = "../../../assets/img/"+imagen)
+  }
 
 }
--- src/app/services/personaje-selector.service.ts
+++ src/app/services/personaje-selector.service.ts
@@ -10,8 +10,21 @@ export class PersonajeSelectorService {
 
   private personaje = new BehaviorSubject<string>("Arquero");
   currentPersonaje = this.personaje.asObservable();
+  private imagen = new BehaviorSubject<string>("robin-hood.png");
+  currentImagen = this.imagen.asObservable();
 
   setPersonaje(newPersonaje:string){
     this.personaje.next(newPersonaje);
+    if(newPersonaje == "Hada"){
+      this.imagen.next("fairy.png");
+    }else if(newPersonaje == "Fantasma"){
+      this.imagen.next("ghost.png");
+    }else if(newPersonaje == "Arquero"){
+      this.imagen.next("robin-hood.png");
+    }else if(newPersonaje == "Bruja"){
+      this.imagen.next("witch.png");
+    }else if(newPersonaje == "Hechicero"){
+      this.imagen.next("wizard.png");
+    }
   }
 }
